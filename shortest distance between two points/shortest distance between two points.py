f8304f85ac0b26c3be09cd93b3da2f10ea6b9909
omarap <patrick.omara7@gmail.com>
2022-03-27 15:50:27 +0300

    first commit

--- shortest distance between two points/shortest distance between two points.py
+++ shortest distance between two points/shortest distance between two points.py
@@ -1,10 +1,10 @@
 points = [(1,5), (5,8), (8,1), (9,5)]
 
-def euclideanDistance(coordinate1, coordinate2):
+def Distance(coordinate1, coordinate2):
     return pow(pow(coordinate1[0] - coordinate2[0], 2) + pow(coordinate1[1] - coordinate2[1], 2), .5)
 
 distances = []
 for i in range(len(points)-1):
     for j in range(i+1, len(points)):
-        distances += [euclideanDistance(points[i],points[j])]
+        distances += [Distance(points[i],points[j])]
 print (min(distances))
